b3aaeef06ddb9edd7dc84e9b49a6b3c56427cf13
Mark Mercado <mamercad@gmail.com>
2021-12-05 13:22:59 -0500

    adding golang/aoc01 part 2

--- golang/aoc01.go
+++ golang/aoc01.go
@@ -7,6 +7,18 @@ import (
 	"strings"
 )
 
+func getWindow(lines []string, index int, length int) int {
+	window := 0
+	for i := 0; i < length; i++ {
+		curVal, err := strconv.Atoi(lines[index+i])
+		if err != nil {
+			panic(err)
+		}
+		window += curVal
+	}
+	return window
+}
+
 func main() {
 
 	file, err := os.ReadFile("aoc01.dat")
@@ -21,12 +33,12 @@ func main() {
 		if i+1 < len(lines) {
 			curVal, err := strconv.Atoi(lines[i])
 			if err != nil {
-				panic(lines[i])
+				panic(err)
 			}
 
 			nexVal, err := strconv.Atoi(lines[i+1])
 			if err != nil {
-				panic(lines[i+1])
+				panic(err)
 			}
 
 			if curVal < nexVal {
@@ -35,5 +47,18 @@ func main() {
 		}
 	}
 
-	fmt.Println(increases)
+	fmt.Printf("part 1 increases: %v\n", increases)
+
+	increases = 0
+	for i := range lines {
+		if i+3 < len(lines) {
+			win1 := getWindow(lines, i, 3)
+			win2 := getWindow(lines, i+1, 3)
+			if win1 < win2 {
+				increases++
+			}
+		}
+	}
+
+	fmt.Printf("part 2 increases: %v\n", increases)
 }
