f942e6e69775deccea31ab1cb9507d25278a97fc
Mark Mercado <mamercad@gmail.com>
2021-12-05 13:22:32 -0500

    adding python/aoc01 part 2

--- python/main.py
+++ python/main.py
@@ -3,9 +3,22 @@
 with open("aoc01.dat") as f:
     lines = f.read().splitlines()
 
+# part 1
+
 increases = 0
 for i in range(len(lines)-1):
     if int(lines[i]) < int(lines[i+1]):
         increases += 1
 
-print(increases)
+print(f"part 1 increases: {increases}")
+
+# part 2
+
+increases = 0
+for i in range(len(lines)-3):
+    window1 = int(lines[i]) + int(lines[i+1]) + int(lines[i+2])
+    window2 = int(lines[i+1]) + int(lines[i+2]) + int(lines[i+3])
+    if window1 < window2:
+        increases += 1
+
+print(f"part 2 increases: {increases}")
