d92d8a7da0d5753f9958d7b2da9d02fe36e66a8d
Mark Mercado <mamercad@gmail.com>
2021-12-06 18:59:08 -0500

    adding python/aoc02 part 2

--- python/aoc02/aoc02.py
+++ python/aoc02/aoc02.py
@@ -3,6 +3,8 @@
 with open("aoc02.dat") as f:
     lines = f.read().splitlines()
 
+# part 1
+
 position = [0, 0]
 for line in lines:
     direction, distance = line.split()
@@ -15,5 +17,26 @@ for line in lines:
     else:
         raise Exception(f"unknown direction: {direction}")
 
+print("part 1")
+print(f"position: {position}")
+print(f"product: {position[0] * position[1]}")
+
+# part 2
+
+position = [0, 0]
+aim = 0
+for line in lines:
+    direction, distance = line.split()
+    if direction == "forward":
+        position[0] += int(distance)
+        position[1] += int(distance) * aim
+    elif direction == "up":
+        aim -= int(distance)
+    elif direction == "down":
+        aim += int(distance)
+    else:
+        raise Exception(f"unknown direction: {direction}")
+
+print("part 2")
 print(f"position: {position}")
 print(f"product: {position[0] * position[1]}")
