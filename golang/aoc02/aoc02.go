d1e7d19daa1d58c89836c6a76d00ea88dbed5611
Mark Mercado <mamercad@gmail.com>
2021-12-06 19:00:45 -0500

    adding golang/aoc02 part 2

--- golang/aoc02/aoc02.go
+++ golang/aoc02/aoc02.go
@@ -16,10 +16,9 @@ func readInput(fileName string) []string {
 	return lines
 }
 
-func computePosition(lines []string) ([]int, int) {
+func computePositionPart1(lines []string) ([]int, int) {
 	coords := []int{0, 0}
 	for _, line := range lines {
-		fmt.Println(line)
 		s := strings.Split(line, " ")
 		direction := s[0]
 		distance, err := strconv.Atoi(s[1])
@@ -38,8 +37,33 @@ func computePosition(lines []string) ([]int, int) {
 	return coords, coords[0] * coords[1]
 }
 
+func computePositionPart2(lines []string) ([]int, int) {
+	coords := []int{0, 0}
+	aim := 0
+	for _, line := range lines {
+		s := strings.Split(line, " ")
+		direction := s[0]
+		distance, err := strconv.Atoi(s[1])
+		if err != nil {
+			panic(err)
+		}
+		switch direction {
+		case "forward":
+			coords[0] += distance
+			coords[1] += distance * aim
+		case "up":
+			aim -= distance
+		case "down":
+			aim += distance
+		}
+	}
+	return coords, coords[0] * coords[1]
+}
+
 func main() {
 	lines := readInput("aoc02.dat")
-	var coords, product = computePosition(lines)
-	fmt.Printf("position: [%v, %v]\nproduct: %v\n", coords[0], coords[1], product)
+	var coords, product = computePositionPart1(lines)
+	fmt.Printf("part 1\nposition: [%v, %v]\nproduct: %v\n", coords[0], coords[1], product)
+	coords, product = computePositionPart2(lines)
+	fmt.Printf("part 2\nposition: [%v, %v]\nproduct: %v\n", coords[0], coords[1], product)
 }
